5504a21b9f936fe3ee28d4ca4f38de5e658e5329
ShlyapaEx <musha742@gmail.com>
2023-04-03 12:17:32 +0300

    решение после разбора

--- Яндекс. Тренировка алгоритмов 1.0/Домашнее задание по лекции 1/I. Prisoner of the castle If.py
+++ Яндекс. Тренировка алгоритмов 1.0/Домашнее задание по лекции 1/I. Prisoner of the castle If.py
@@ -1,22 +1,10 @@
 # https://contest.yandex.ru/contest/27393/problems/I/
-
-
-def try_sides(side1, side2, hole_side1, hole_side2):
-
-    if (side1 <= hole_side1 and side2 <= hole_side2) or \
-            (side1 <= hole_side2 and side2 <= hole_side1):
-        return True
-    return False
-
+# Решение на основе разбора: https://www.youtube.com/watch?v=mdJdB7On4AM&list=PL6Wui14DvQPySdPv5NUqV3i8sDbHkCKC5&index=5
 
 def main():
-    A, B, C, D, E = [int(input()) for _ in range(5)]
-
-    try1 = try_sides(A, B, D, E)
-    try2 = try_sides(A, C, D, E)
-    try3 = try_sides(B, C, D, E)
-
-    if any((try1, try2, try3)):
+    brick, hole = sorted([int(input()) for _ in range(3)]), \
+        sorted([int(input()) for _ in range(2)])
+    if hole[0] >= brick[0] and hole[1] >= brick[1]:
         return "YES"
     return "NO"
 
