f78ffdcc567efed272590a6a90fc35d88860a066
ShlyapaEx <musha742@gmail.com>
2023-07-02 13:14:59 +0300

    ДЗ №3 конец

--- Яндекс. Тренировка алгоритмов 1.0/Домашнее задание по лекции 3/I. Polyglots.py
+++ Яндекс. Тренировка алгоритмов 1.0/Домашнее задание по лекции 3/I. Polyglots.py
@@ -1,43 +1,44 @@
 # https://contest.yandex.ru/contest/27663/problems/I/
 
-def sort_languages(students_count: int, students: list[tuple[str]]):
-    pass
+def sort_languages_easy(_, students: list[set[str]]) -> tuple[set[str], set[str]]:
+    intersected_languages = students[0].intersection(*students)
+    all_languages = students[0].union(*students)
 
+    return (intersected_languages, all_languages)
+
+
+def sort_languages_hard(students_count: int, students: list[set[str]]) -> tuple[list[str], list[str]]:
+    languages_with_count = {}
+    intersected_languages = []
+    all_languages = []
+
+    for i in range(students_count):
+        for language in students[i]:
+            if language in languages_with_count:
+                languages_with_count[language] += 1
+            else:
+                languages_with_count[language] = 1
+  
+    for language, students_know_count in languages_with_count.items():
+        if students_count == students_know_count:
+            intersected_languages.append(language)
+        all_languages.append(language)
+
+    return (intersected_languages, all_languages)
 
 def main():
     students_count = int(input())
     students = []
     for _ in range(students_count):
         languages_count = int(input())
-        students.append(tuple(input() for _ in range(languages_count)))
-    result = sort_languages(students_count, students)
-
-
-def fuck():
-    students_count = int(input())
-    languages = {}
-    languages_known_by_all = []
-    left_languages = []
-    for _ in range(students_count):
-        language_count = int(input())
-        for _ in range(language_count):
-            new_language = input()
-            if new_language in languages.keys():
-                languages[new_language] += 1
-            else:
-                languages[new_language] = 1
-    for key, item in languages.items():
-        if item == students_count:
-            languages_known_by_all.append(key)
-        left_languages.append(key)
-
-    print(len(languages_known_by_all))
-    for i in range(len(languages_known_by_all)):
-        print(languages_known_by_all[i])
-    print(len(left_languages))
-    for i in range(len(left_languages)):
-        print(left_languages[i])
+        students.append({input() for _ in range(languages_count)})
+    results = sort_languages_easy(students_count, students)
+    
+    for result in results:
+        print(len(result))
+        for language in result:
+            print(language)
 
 
 if __name__ == '__main__':
-    fuck()
+    main()
