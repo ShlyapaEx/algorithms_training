7552dd310b84664c27db7f387e2997c0695ed24e
ShlyapaEx <musha742@gmail.com>
2023-07-02 13:24:33 +0300

    j - cheat_checking cleanup

--- Яндекс. Тренировка алгоритмов 1.0/Домашнее задание по лекции 4/J. Cheat checking.py
+++ Яндекс. Тренировка алгоритмов 1.0/Домашнее задание по лекции 4/J. Cheat checking.py
@@ -4,38 +4,17 @@ import re
 from typing import Literal
 
 
-def keywithmaxval(words: dict):
-    most_frequent_word = ''
-    most_frequent_word_count = 0
-    most_frequent_word_first_line = 0
-    most_frequent_word_first_row = 0
-    for word in words:
-        if words[word]['word_count'] > most_frequent_word_count:
-            most_frequent_word = word
-            most_frequent_word_count = words[word]['word_count']
-            most_frequent_word_first_line = words[word]['first_met_line']
-            most_frequent_word_first_row = words[word]['first_word_number']
-        elif words[word]['word_count'] == most_frequent_word_count:
-            if words[word]['first_met_line'] < most_frequent_word_first_line \
-                    or words[word]['first_word_number'] < most_frequent_word_first_row:
-                most_frequent_word = word
-                most_frequent_word_count = words[word]['word_count']
-                most_frequent_word_first_line = words[word]['first_met_line']
-                most_frequent_word_first_row = words[word]['first_word_number']
-    return most_frequent_word
+def keywithmaxval(d: dict):
+    v = list(d.values())
+    k = list(d.keys())
+    return k[v.index(max(v))]
 
 
 def is_identifier(word: str, control_words: set, is_case_sensitive: bool,
                   can_start_with_number: bool) -> bool:
-    if word.isnumeric():
-        return False
-    if not can_start_with_number:
-        if word[0].isnumeric():
-            return False
-    if not is_case_sensitive:
-        if word.lower() in control_words:
-            return False
-    elif word in control_words:
+    if word.isnumeric() or (not can_start_with_number and word[0].isnumeric()) \
+        or word in control_words \
+            or (not is_case_sensitive and word.lower() in control_words):
         return False
     return True
 
@@ -43,7 +22,6 @@ def is_identifier(word: str, control_words: set, is_case_sensitive: bool,
 def additional_cheat_checking(text: list[str], control_words: set,
                               is_case_sensitive: bool, can_start_with_number: bool):
     identifiers_with_count = {}
-    # most_frequent_identifier_count = 0
 
     for line_number, line in enumerate(text):
         for word_number, word in enumerate(line.split()):
@@ -52,57 +30,14 @@ def additional_cheat_checking(text: list[str], control_words: set,
             if is_identifier(word, control_words,
                              is_case_sensitive, can_start_with_number):
                 if word not in identifiers_with_count:
-                    identifiers_with_count[word] = {'word_count': 0,
-                                                    'first_met_line': line_number,
-                                                    'first_word_number': word_number}
-                identifiers_with_count[word]['word_count'] += 1
+                    identifiers_with_count[word] = 0
+                identifiers_with_count[word] += 1
 
     return keywithmaxval(identifiers_with_count)
 
 
 def yes_no_to_bool(string: Literal['yes', 'no']) -> bool:
     return True if string == 'yes' else False
-
-
-# assert additional_cheat_checking(set(), True, False, '''int main() {
-#   int a;
-#   int b;
-#   scanf("%d%d", &a, &b);
-#   printf("%d", a + b);
-# }'''.split()) == 'int'
-# assert additional_cheat_checking(set(), True, False, '''#define INT int
-# int main() {
-#   INT a, b;
-#   scanf("%d%d", &a, &b);
-#   printf("%d %d", a + b, 0);
-# }'''.split()) == 'd'
-# assert additional_cheat_checking(set(), False, False, '''#define INT int
-# int main() {
-#   INT a, b;
-#   scanf("%d%d", &a, &b);
-#   printf("%d %d", a + b, 0);
-# }'''.split()) == 'int'
-# assert additional_cheat_checking({'program', 'var', 'begin', 'end', 'while', 'for'}, False, False, '''program sum;
-# var
-#   A, B: integer;
-# begin
-#   read(A, b);
-#   writeln(a + b);
-# end.'''.split()) == 'a'
-# assert additional_cheat_checking({'_'}, True, True, '''a = 0h
-# b = 0h
-# c = 0h'''.split()) == '0h'
-# assert additional_cheat_checking({'a'}, False, False, '''A b a a a
-# b b b B a'''.split()) == 'b'
-# assert additional_cheat_checking({'b'}, False, False, '''A b a a a
-# b b b B a'''.split()) == 'a'
-# assert additional_cheat_checking({'a', 'b'}, True, False, '''A b a a a
-# b b b B a'''.split()) == 'A'
-# assert additional_cheat_checking(
-#     set(), False, False, '''a b c c'''.split()) == 'c'
-# assert additional_cheat_checking(set(), True, False, '''3abc 3abc 3abc
-# AbC  AbC  AbC
-# abc  abc  abc'''.split()) == 'AbC'
 
 
 def main():
